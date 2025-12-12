2808f26941464141e5f1158305e51bb721307636
Ayo-Oub <ayoub.cllg@gmail.com>
2025-12-12 22:14:28 +0100

    add codes

--- src/pages/contents/SelectEleDom.jsx
+++ src/pages/contents/SelectEleDom.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import './styling_cours.css'
+
 
 export default function SelectEleDom() {
   return (
-    <div>
-      <h2>Cours 2</h2>
+    <div className="content-container">
+      <h3 className="title">Sélectionner des éléments dans le DOM</h3>
+      <p>Ce chapitre est le plus important parce que si tu ne sais pas sélectionner un élément, tu ne pourra pas jamais le modifier ou le supprimer.</p>
+      <p>Il existe cinq grandes méthodes pour sélectionner des éléments dans le DOM.</p>
+      <h4 className="sub-title">1. getElementById()</h4>
+      <p>Sélectionne un seul élément par son <span>id</span></p>
     </div>
   )
 }
