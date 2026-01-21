e4ae686d4961eb4a13252260731e89fc0f5ef826
Ayo-Oub <ayoub.cllg@gmail.com>
2026-01-21 12:16:51 +0100

    fix router github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from '../src/pages/Home'
-import IntroDom from './pages/contents/IntroDom'
-import SelectEleDom from './pages/contents/SelectEleDom'
-function App() {
-  const route = createBrowserRouter([
-    {
-      path: "/learn_js",
-      element: <Home />,
-      children: [
-        { path: "intro_dom", element: <IntroDom /> },
-        { path: "select_element_in_the_dom", element: <SelectEleDom /> },
-      ],
-    },
-  ]);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import IntroDom from "./pages/contents/IntroDom";
+import SelectEleDom from "./pages/contents/SelectEleDom";
 
-return <RouterProvider router={route}/>
+function App() {
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+        children: [
+          { path: "intro_dom", element: <IntroDom /> },
+          { path: "select_element_in_the_dom", element: <SelectEleDom /> },
+        ],
+      },
+    ],
+    {
+      basename: "/learn_js",
+    },
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
